723a9b9c4ed82406f2b0b0b8548432afba3cb106
thousand-three <1403159549@qq.com>
2021-06-02 13:50:06 +0800

    添加头文件和源文件

---
+++ thousand/thousand.cpp
@@ -0,0 +1,92 @@
+#include "thousand.h"
+
+std::vector<std::string> split(std::string str, char ch)
+{
+    std::string sub_str = "";
+    std::vector<std::string> result;
+    result.reserve(str.length() / 2);
+    for (auto s : str)
+    {
+        if (s == ch)
+        {
+            result.push_back(sub_str);
+            sub_str = "";
+        }
+        else
+        {
+            sub_str += s;
+        }
+    }
+    result.push_back(sub_str);
+    return result;
+}
+
+std::string lstrip(std::string str, char ch)
+{
+    std::size_t index = 0;
+    for (auto s : str)
+    {
+        if (s == ch)
+        {
+            ++index;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (index >= str.length())
+    {
+        return "";
+    }
+    return str.substr(index);
+}
+
+std::string rstrip(std::string str, char ch)
+{
+    std::size_t index = str.length() - 1;
+    while (index >= 0 && str[index] == ch)
+    {
+        --index;
+    }
+    if (index < 0)
+    {
+        return "";
+    }
+    return str.substr(0, index + 1);
+}
+
+std::string replace(std::string str, std::string srce, std::string dest, int limit)
+{
+    std::size_t content_len = srce.length();
+    if (limit == 0)
+    {
+        while (1)
+        {
+            std::size_t index = str.find(srce);
+            if (index == std::string::npos)
+            {
+                break;
+            }
+            else
+            {
+                str = str.replace(index, content_len, dest);
+            }
+        }
+        return str;
+    }
+    while (limit > 0)
+    {
+        std::size_t index = str.find(srce);
+        if (index == std::string::npos)
+        {
+            break;
+        }
+        else
+        {
+            str = str.replace(index, content_len, dest);
+        }
+        --limit;
+    }
+    return str;
+}
---
+++ thousand/thousand.h
@@ -0,0 +1,31 @@
+#ifndef _THOUSAND_H_
+#define _THOUSAND_H_
+
+#include <vector>
+#include <string>
+
+//字符串分割，按照指定字符将字符串分割为多个子串
+//@parameter str 原字符串
+//@parameter ch 进行分割的标记字符
+std::vector<std::string> split(std::string str, char ch);
+
+//删除字符串从开始位置的指定字符
+//@parameter str 原字符串
+//@parameter ch 要删除的字符
+std::string lstrip(std::string str, char ch);
+
+//删除字符串从末尾位置开始的指定字符
+//@parameter str 原字符串
+//@parameter ch 要删除的字符
+std::string rstrip(std::string str, char ch);
+
+//从开始位置，替换字符串中指定的内容
+//@parameter str 要替换的字符串
+//@parameter srce 被替换的内容
+//@parameter dest 替换的内容
+//@parameter limit 要替换的次数，默认为0，表示全部替换
+std::string replace(std::string str, std::string srce, std::string dest, int limit = 0);
+
+
+
+#endif
